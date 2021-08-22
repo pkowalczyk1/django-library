d5a0ecbfba978329a9b2c80153f320133688cdab
pkowalczyk1 <piotrek12kowalczyk@gmail.com>
2021-08-22 09:34:40 +0200

    Minor changes to configuration

--- libraryproject/settings.py
+++ libraryproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qmtr%2g@b(1uuw$q%+#j@usvic^sj(cbrjp15i3g%m8hdcus2#'
+SECRET_KEY = secret_key
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
